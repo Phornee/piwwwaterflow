42d64d32bde2921862a974e87bc7a8df2ba04ea7
Phornee <phornee@gmail.com>
2020-12-15 15:38:42 +0100

    favicon, Status, time input

--- apt.py
+++ apt.py
@@ -5,14 +5,16 @@ import yaml
 import datetime
 
 app = Flask(__name__)
- 
+
+
 # mainpage
 @app.route('/')
 def index():
     return 'This is the Pi server.'
 
+
 # log
-@app.route('/log')
+@app.route('/log', methods=['GET'])
 def log():
     log_string = ''
 
@@ -26,11 +28,10 @@ def log():
     response.boy = log_string
     return response
 
-@app.route('/config',methods=['GET', 'POST'])
+
+@app.route('/config', methods=['GET'])
 def config():
     if request.method == 'GET':
-        #nombreUser = request.args.get('nombreUser')
-
         file_folder = Path(__file__).parent
         config_log = os.path.join(file_folder, '../PiWaterflow/config.yml')
 
@@ -41,41 +42,36 @@ def config():
         response.boy = log_string
         return response
 
-        #return "<h1>GET " + nombreUser + "</h1>"
-    elif  request.method == 'POST':
-        language = request.form.get('language')
-        framework = request.form['framework']
 
-        return '''<h1>The language value is: {}</h1>
-                  <h1>The framework value is: {}</h1>'''.format(language, framework)
-
-    return '''<form method="POST">
-                  Language: <input type="text" name="language"><br>
-                  Framework: <input type="text" name="framework"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
-
-@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])  # allow both GET and POST requests
 def form_example():
-    if request.method == 'POST':  #this block is only entered when the form is submitted
+    if request.method == 'POST':  # this block is only entered when the form is submitted
         file_folder = Path(__file__).parent
         config_yml_path = os.path.join(file_folder, '../PiWaterflow/config.yml')
 
         with open(config_yml_path, 'r') as config_file:
             config = yaml.load(config_file, Loader=yaml.FullLoader)
 
-        config['programs'][0]['start_time'] = datetime.datetime.strptime(config['programs'][0]['start_time'], '%H:%M:%S')
-        new_datetime = config['programs'][0]['start_time'].replace(hour=int(request.form.get('hour1')),
-                                                                       minute=int(request.form.get('minute1')))
+        config['programs'][0]['start_time'] = datetime.datetime.strptime(config['programs'][0]['start_time'],
+                                                                         '%H:%M:%S')
+        time1 = datetime.datetime.strptime(request.form.get('time1'), '%H:%M')
+        new_datetime = config['programs'][0]['start_time'].replace(hour=time1.hour, minute=time1.minute)
         config['programs'][0]['start_time'] = new_datetime.strftime('%H:%M:%S')
         config['programs'][0]['valves_times'][0] = int(request.form.get('valve11'))
         config['programs'][0]['valves_times'][1] = int(request.form.get('valve12'))
 
+        config['programs'][1]['start_time'] = datetime.datetime.strptime(config['programs'][1]['start_time'],
+                                                                         '%H:%M:%S')
+        time2 = datetime.datetime.strptime(request.form.get('time2'), '%H:%M')
+        new_datetime = config['programs'][1]['start_time'].replace(hour=time2.hour, minute=time2.minute)
+        config['programs'][1]['start_time'] = new_datetime.strftime('%H:%M:%S')
+        config['programs'][1]['valves_times'][0] = int(request.form.get('valve21'))
+        config['programs'][1]['valves_times'][1] = int(request.form.get('valve22'))
+
         with open(config_yml_path, 'w') as config_file:
             yaml.dump(config, config_file)
 
-        return redirect(url_for('form_example')) #Redirect so that we dont RE-POST same data again when refreshing
-
+        return redirect(url_for('form_example'))  # Redirect so that we dont RE-POST same data again when refreshing
 
     file_folder = Path(__file__).parent
     config_yml_path = os.path.join(file_folder, '../PiWaterflow/config.yml')
@@ -86,11 +82,31 @@ def form_example():
         for program in config['programs']:
             program['start_time'] = datetime.datetime.strptime(program['start_time'], '%H:%M:%S')
 
-    return render_template('form.html', hour1=config['programs'][0]['start_time'].hour
-                                      , minute1=config['programs'][0]['start_time'].minute
-                                      , valve11=config['programs'][0]['valves_times'][0]
-                                      , valve12=config['programs'][0]['valves_times'][1])
+        # Sort the programs by time
+        config['programs'].sort(key=lambda prog: prog['start_time'])
 
+    import psutil
+    found = False
+    for i in psutil.process_iter():
+        try:
+            cmdline = i.cmdline()
+            if cmdline[0].find('python') != -1 and cmdline[1].find('rele.py') != -1:
+                found = True
+                break
+        except Exception as e:
+            pass
+
+    return render_template('form.html'
+                           , time1=("{:02}:{:02}".format(config['programs'][0]['start_time'].hour,
+                                                         config['programs'][0]['start_time'].minute))
+                           , valve11=config['programs'][0]['valves_times'][0]
+                           , valve12=config['programs'][0]['valves_times'][1]
+                           , time2=("{:02}:{:02}".format(config['programs'][1]['start_time'].hour,
+                                                         config['programs'][1]['start_time'].minute))
+                           , valve21=config['programs'][1]['valves_times'][0]
+                           , valve22=config['programs'][1]['valves_times'][1]
+                           , looprunning=found
+                           )
 
 
 if __name__ == '__main__':
