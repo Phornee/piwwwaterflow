f2bffabf2a7c8368aa68c601053bd92bb9315cbc
Phornee <phornee@gmail.com>
2021-01-24 16:03:53 +0100

    Refactor webservice to piwaterflow

--- piwwwaterflow/wsgi.py
+++ piwwwaterflow/wsgi.py
@@ -1,4 +1,8 @@
-from webservice import app
+from piwaterflow import app
+from pathlib import Path
 
 if __name__ == "__main__":
+    wsgi_path = Path(__file__).parent
+    app.template_folder = "{}/templates".format(wsgi_path)
+    app.static_folder = "{}/static".format(wsgi_path)
     app.run()
