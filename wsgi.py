a0ef687196820e166b823c855d823a4a44bb6a25
Phornee <phornee@gmail.com>
2021-01-30 18:14:12 +0100

    flask in class

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
-from piwaterflow import app
+from PiWaterflow import PiWWWaterflowService
 from pathlib import Path
 
 if __name__ == "__main__":
-    wsgi_path = Path(__file__).parent
-    app.template_folder = "{}/templates".format(wsgi_path)
-    app.static_folder = "{}/static".format(wsgi_path)
-    app.run()
+    current_path = "{}/templates".format(Path().absolute())
+    flask_app = PiWWWaterflowService(template_folder=current_path)
+    flask_app.run()
