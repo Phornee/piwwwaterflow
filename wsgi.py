d47b3b74e45e443421827affc32ef2e1facc1451
Phornee <phornee@gmail.com>
2021-02-01 11:57:49 +0100

    app

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,8 @@ def create_app():
     flask_app = PiWWWaterflowService(template_folder=current_path)
     return flask_app
 
-
+if __name__ == '__main__':
+    create_app = create_app()
+    create_app.run()
+else:
+    gunicorn_app = create_app()
