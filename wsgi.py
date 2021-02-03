539eb2a49ca19a5d7b4754b91d3ce71ffae11ffc
Phornee <phornee@gmail.com>
2021-02-03 10:08:09 +0100

    sizes in html

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,11 @@ from piwaterflow import PiWWWaterflowService
 from pathlib import Path
 
 def create_app():
-    current_path = "{}/templates".format(Path().absolute())
-    wtf_service = PiWWWaterflowService(template_folder=current_path)
+    templates_path = "{}/templates".format(Path().absolute())
+    static_path = "{}/static".format(Path().absolute())
+    wtf_service = PiWWWaterflowService(template_folder=templates_path, static_folder=static_path)
     return wtf_service.getApp()
 
-# if __name__ == '__main__':
-#     create_app = create_app()
-#     create_app.run()
-# else:
-#     gunicorn_app = create_app()
+if __name__ == '__main__':
+    create_app = create_app()
+    create_app.run()
