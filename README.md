f49bbbd9febac81a2640944c2a52f13737531d5b
Phornee <phornee@gmail.com>
2021-11-30 10:29:38 +0100

    .

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 Flask/Gunicorn Webservice for piwaterflow system
 
 + 1.1.0
-    + Avoid service caching by adding timestamp to URL
+  + Avoid service caching by adding timestamp to URL
++ 1.1.1
+  + Point to new grafana 
++ 1.2.2
+  + New waterflow config & LAN influxdb
 
--- static/js/code.js
+++ static/js/code.js
@@ -92,7 +92,7 @@ function update(first_time){
     requestservice.onload = function() {
         // Version label update
         var versionlabel = document.getElementById('version');
-        frontend = '1.2.1'
+        frontend = '1.2.2'
         backend = requestservice.response.version
         versionlabel.textContent = `PiWaterflow ${frontend} (Backend ${backend})`
 
