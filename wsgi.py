4128e7a9c1311decbbc415a5590db1ebe47a591f
Phornee <phornee@gmail.com>
2022-12-23 09:04:15 +0100

    .

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 from piwaterflow import PiWWWaterflowService
 from pathlib import Path
 
+
 def create_app():
     templates_path = "{}/templates".format(Path().absolute())
     static_path = "{}/static".format(Path().absolute())
     wtf_service = PiWWWaterflowService(template_folder=templates_path, static_folder=static_path)
     return wtf_service.getApp()
+
 
 if __name__ == '__main__':
     create_app = create_app()
