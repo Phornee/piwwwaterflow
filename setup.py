98543f7e4f4d76c2e55a632dcfbed0d44c54980d
Phornee <phornee@gmail.com>
2023-05-08 08:46:54 +0200

    include package data

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Phornee/piwwwaterflow",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     package_data={
         'static.css': ['*.css'],
         'static.img': ['*.*'],
